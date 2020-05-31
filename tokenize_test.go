1684faa06c037bda0adb1b668d39b8acd77ac398
irenicaa <root@irenica.pro>
2020-05-31 03:17:52 +0300

    ...

--- tokenize_test.go
+++ tokenize_test.go
@@ -16,7 +16,44 @@ func TestTokenize(test *testing.T) {
 		args       args
 		wantTokens []Token
 		wantErr    string
-	}{}
+	}{
+		{
+			name:       "integer",
+			args:       args{code: "23"},
+			wantTokens: []Token{{Kind: NumberToken, Value: "23"}},
+			wantErr:    "",
+		},
+		{
+			name:       "fractional",
+			args:       args{code: ".23"},
+			wantTokens: []Token{{Kind: NumberToken, Value: ".23"}},
+			wantErr:    "",
+		},
+		{
+			name:       "integer and fractional",
+			args:       args{code: "23.42"},
+			wantTokens: []Token{{Kind: NumberToken, Value: "23.42"}},
+			wantErr:    "",
+		},
+		{
+			name:       "exponent",
+			args:       args{code: "23.42e6"},
+			wantTokens: []Token{{Kind: NumberToken, Value: "23.42e6"}},
+			wantErr:    "",
+		},
+		{
+			name:       "exponent with plus",
+			args:       args{code: "23.42e+10"},
+			wantTokens: []Token{{Kind: NumberToken, Value: "23.42e+10"}},
+			wantErr:    "",
+		},
+		{
+			name:       "exponent with minus",
+			args:       args{code: "23.42e-10"},
+			wantTokens: []Token{{Kind: NumberToken, Value: "23.42e-10"}},
+			wantErr:    "",
+		},
+	}
 	for _, testCase := range testsCases {
 		test.Run(testCase.name, func(test *testing.T) {
 			gotTokens, gotErr := Tokenize(testCase.args.code)
