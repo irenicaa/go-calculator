cd11791cf14c02d971d02d96d675faffb479317c
irenicaa <root@irenica.pro>
2020-06-25 17:48:57 +0300

    ...

---
+++ translate_test.go
@@ -0,0 +1,32 @@
+package calculator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTranslate(test *testing.T) {
+	type args struct {
+		tokens    []Token
+		functions map[string]struct{}
+	}
+
+	testsCases := []struct {
+		name         string
+		args         args
+		wantCommands []Command
+		wantErr      string
+	}{}
+	for _, testCase := range testsCases {
+		test.Run(testCase.name, func(test *testing.T) {
+			gotCommands, gotErr := Translate(
+				testCase.args.tokens,
+				testCase.args.functions,
+			)
+
+			assert.Equal(test, testCase.wantCommands, gotCommands)
+			assert.Equal(test, testCase.wantErr, gotErr)
+		})
+	}
+}
