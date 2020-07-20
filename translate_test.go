c13ce08197762245b883b1673da40551467b235d
irenicaa <root@irenica.pro>
2020-07-20 18:43:15 +0300

    ...

--- translate_test.go
+++ translate_test.go
@@ -17,7 +17,204 @@ func TestTranslate(test *testing.T) {
 		args         args
 		wantCommands []Command
 		wantErr      string
-	}{}
+	}{
+		{
+			name: "number",
+			args: args{
+				tokens:    []Token{{Kind: NumberToken, Value: "23"}},
+				functions: nil,
+			},
+			wantCommands: []Command{{Kind: PushNumberCommand, Operand: "23"}},
+			wantErr:      "",
+		},
+		{
+			name: "identifier",
+			args: args{
+				tokens:    []Token{{Kind: IdentifierToken, Value: "test"}},
+				functions: nil,
+			},
+			wantCommands: []Command{{Kind: PushVariableCommand, Operand: "test"}},
+			wantErr:      "",
+		},
+		{
+			name: "few operators with the same precedence",
+			args: args{
+				tokens: []Token{
+					{Kind: NumberToken, Value: "12"},
+					{Kind: PlusToken, Value: "+"},
+					{Kind: NumberToken, Value: "23"},
+					{Kind: MinusToken, Value: "-"},
+					{Kind: NumberToken, Value: "42"},
+				},
+				functions: nil,
+			},
+			wantCommands: []Command{
+				{Kind: PushNumberCommand, Operand: "12"},
+				{Kind: PushNumberCommand, Operand: "23"},
+				{Kind: CallFunctionCommand, Operand: "+"},
+				{Kind: PushNumberCommand, Operand: "42"},
+				{Kind: CallFunctionCommand, Operand: "-"},
+			},
+			wantErr: "",
+		},
+		{
+			name: "few operators with different precedences (ascending)",
+			args: args{
+				tokens: []Token{
+					{Kind: NumberToken, Value: "12"},
+					{Kind: PlusToken, Value: "+"},
+					{Kind: NumberToken, Value: "23"},
+					{Kind: AsteriskToken, Value: "*"},
+					{Kind: NumberToken, Value: "42"},
+				},
+				functions: nil,
+			},
+			wantCommands: []Command{
+				{Kind: PushNumberCommand, Operand: "12"},
+				{Kind: PushNumberCommand, Operand: "23"},
+				{Kind: PushNumberCommand, Operand: "42"},
+				{Kind: CallFunctionCommand, Operand: "*"},
+				{Kind: CallFunctionCommand, Operand: "+"},
+			},
+			wantErr: "",
+		},
+		{
+			name: "few operators with different precedences (descending)",
+			args: args{
+				tokens: []Token{
+					{Kind: NumberToken, Value: "12"},
+					{Kind: AsteriskToken, Value: "*"},
+					{Kind: NumberToken, Value: "23"},
+					{Kind: PlusToken, Value: "+"},
+					{Kind: NumberToken, Value: "42"},
+				},
+				functions: nil,
+			},
+			wantCommands: []Command{
+				{Kind: PushNumberCommand, Operand: "12"},
+				{Kind: PushNumberCommand, Operand: "23"},
+				{Kind: CallFunctionCommand, Operand: "*"},
+				{Kind: PushNumberCommand, Operand: "42"},
+				{Kind: CallFunctionCommand, Operand: "+"},
+			},
+			wantErr: "",
+		},
+		{
+			name: "few operators with one pair of parentheses",
+			args: args{
+				tokens: []Token{
+					{Kind: LeftParenthesisToken, Value: "("},
+					{Kind: NumberToken, Value: "12"},
+					{Kind: PlusToken, Value: "+"},
+					{Kind: NumberToken, Value: "23"},
+					{Kind: RightParenthesisToken, Value: ")"},
+					{Kind: AsteriskToken, Value: "*"},
+					{Kind: NumberToken, Value: "42"},
+				},
+				functions: nil,
+			},
+			wantCommands: []Command{
+				{Kind: PushNumberCommand, Operand: "12"},
+				{Kind: PushNumberCommand, Operand: "23"},
+				{Kind: CallFunctionCommand, Operand: "+"},
+				{Kind: PushNumberCommand, Operand: "42"},
+				{Kind: CallFunctionCommand, Operand: "*"},
+			},
+			wantErr: "",
+		},
+		{
+			name: "few operators with few pairs of parentheses",
+			args: args{
+				tokens: []Token{
+					{Kind: LeftParenthesisToken, Value: "("},
+					{Kind: LeftParenthesisToken, Value: "("},
+					{Kind: NumberToken, Value: "5"},
+					{Kind: PlusToken, Value: "+"},
+					{Kind: NumberToken, Value: "12"},
+					{Kind: RightParenthesisToken, Value: ")"},
+					{Kind: AsteriskToken, Value: "*"},
+					{Kind: NumberToken, Value: "23"},
+					{Kind: RightParenthesisToken, Value: ")"},
+					{Kind: ExponentiationToken, Value: "^"},
+					{Kind: NumberToken, Value: "42"},
+				},
+				functions: nil,
+			},
+			wantCommands: []Command{
+				{Kind: PushNumberCommand, Operand: "5"},
+				{Kind: PushNumberCommand, Operand: "12"},
+				{Kind: CallFunctionCommand, Operand: "+"},
+				{Kind: PushNumberCommand, Operand: "23"},
+				{Kind: CallFunctionCommand, Operand: "*"},
+				{Kind: PushNumberCommand, Operand: "42"},
+				{Kind: CallFunctionCommand, Operand: "^"},
+			},
+			wantErr: "",
+		},
+
+		// error
+		{
+			name: "missed left parenthesis",
+			args: args{
+				tokens: []Token{
+					{Kind: NumberToken, Value: "12"},
+					{Kind: PlusToken, Value: "+"},
+					{Kind: NumberToken, Value: "12"},
+					{Kind: RightParenthesisToken, Value: ")"},
+					{Kind: AsteriskToken, Value: "*"},
+					{Kind: NumberToken, Value: "42"},
+				},
+				functions: nil,
+			},
+			wantCommands: nil,
+			wantErr:      "missed pair for token {Kind:9 Value:)} with number #3",
+		},
+		{
+			name: "missed left parenthesis in a function call",
+			args: args{
+				tokens: []Token{
+					{Kind: IdentifierToken, Value: "test"},
+					{Kind: NumberToken, Value: "23"},
+					{Kind: CommaToken, Value: ","},
+					{Kind: NumberToken, Value: "42"},
+					{Kind: RightParenthesisToken, Value: ")"},
+				},
+				functions: nil,
+			},
+			wantCommands: nil,
+			wantErr:      "missed pair for token {Kind:9 Value:)} with number #4",
+		},
+		{
+			name: "missed function name and left parenthesis in a function call",
+			args: args{
+				tokens: []Token{
+					{Kind: NumberToken, Value: "23"},
+					{Kind: CommaToken, Value: ","},
+					{Kind: NumberToken, Value: "42"},
+					{Kind: RightParenthesisToken, Value: ")"},
+				},
+				functions: nil,
+			},
+			wantCommands: nil,
+			wantErr:      "missed pair for token {Kind:10 Value:,} with number #1",
+		},
+		{
+			name: "missed right parenthesis",
+			args: args{
+				tokens: []Token{
+					{Kind: LeftParenthesisToken, Value: "("},
+					{Kind: NumberToken, Value: "12"},
+					{Kind: PlusToken, Value: "+"},
+					{Kind: NumberToken, Value: "23"},
+					{Kind: AsteriskToken, Value: "*"},
+					{Kind: NumberToken, Value: "42"},
+				},
+				functions: nil,
+			},
+			wantCommands: nil,
+			wantErr:      "missed pair for token {Kind:8 Value:(}",
+		},
+	}
 	for _, testCase := range testsCases {
 		test.Run(testCase.name, func(test *testing.T) {
 			gotCommands, gotErr := Translate(
