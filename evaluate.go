8b432501beba342d4665df040d5d83966822bf60
irenicaa <root@irenica.pro>
2020-08-06 11:57:51 +0300

    ...

--- evaluate.go
+++ evaluate.go
@@ -1,13 +1,28 @@
 package calculator
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
 
 // Evaluate ...
 func Evaluate(commands []Command) (float64, error) {
 	stack := NumberStack{}
-	for _, command := range commands {
+	for commandIndex, command := range commands {
 		switch command.Kind {
 		case PushNumberCommand:
+			number, err := strconv.ParseFloat(command.Operand, 64)
+			if err != nil {
+				return 0, fmt.Errorf(
+					"incorrect number for command %+v with number #%d: %s",
+					command,
+					commandIndex,
+					err,
+				)
+			}
+
+			stack.Push(number)
 		case PushVariableCommand:
 		case CallFunctionCommand:
 		}
@@ -15,7 +30,7 @@ func Evaluate(commands []Command) (float64, error) {
 
 	number, ok := stack.Pop()
 	if !ok {
-		return 0, fmt.Errorf("number stack is empty")
+		return 0, errors.New("number stack is empty")
 	}
 
 	return number, nil
