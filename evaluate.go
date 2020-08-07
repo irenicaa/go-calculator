b102afc826d7244e52565e7d052aa0f0d6c512a1
irenicaa <root@irenica.pro>
2020-08-07 17:26:27 +0300

    ...

--- evaluate.go
+++ evaluate.go
@@ -13,7 +13,11 @@ type Function struct {
 }
 
 // Evaluate ...
-func Evaluate(commands []Command, variables map[string]float64) (float64, error) {
+func Evaluate(
+	commands []Command,
+	variables map[string]float64,
+	functions map[string]Function,
+) (float64, error) {
 	stack := NumberStack{}
 	for commandIndex, command := range commands {
 		switch command.Kind {
@@ -41,6 +45,37 @@ func Evaluate(commands []Command, variables map[string]float64) (float64, error)
 
 			stack.Push(number)
 		case CallFunctionCommand:
+			function, ok := functions[command.Operand]
+			if !ok {
+				return 0, fmt.Errorf(
+					"unknown function in command %+v with number #%d",
+					command,
+					commandIndex,
+				)
+			}
+
+			arguments := []float64{}
+			for argumentIndex := 0; argumentIndex <= function.Arity; argumentIndex++ {
+				number, ok := stack.Pop()
+				if !ok {
+					return 0, fmt.Errorf(
+						"number stack is empty for argument #%d in command %+v with number #%d",
+						argumentIndex,
+						command,
+						commandIndex,
+					)
+				}
+
+				arguments = append(arguments, number)
+			}
+
+			for i := 0; i <= len(arguments)/2; i++ {
+				arguments[function.Arity-i-1], arguments[i] =
+					arguments[i], arguments[function.Arity-i-1]
+			}
+
+			number := function.Handler(arguments)
+			stack.Push(number)
 		}
 	}
 
