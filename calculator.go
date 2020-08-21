9b2d946f8130dae872188debc1c2ef808beb2713
irenicaa <root@irenica.pro>
2020-08-22 02:18:39 +0300

    ...

--- calculator.go
+++ calculator.go
@@ -1,7 +1,10 @@
 package calculator
 
 import (
+	"fmt"
+
 	"github.com/irenicaa/go-calculator/evaluator"
+	"github.com/irenicaa/go-calculator/models"
 	"github.com/irenicaa/go-calculator/tokenizer"
 	"github.com/irenicaa/go-calculator/translator"
 )
@@ -11,4 +14,29 @@ type Calculator struct {
 	tokenizer  tokenizer.Tokenizer
 	translator translator.Translator
 	evaluator  evaluator.Evaluator
+}
+
+// Calculate ...
+func (calculator *Calculator) Calculate(
+	code string,
+	variables map[string]float64,
+	functionsNames map[string]struct{},
+	functions map[string]models.Function,
+) error {
+	tokens, err := calculator.tokenizer.Tokenize(code)
+	if err != nil {
+		return fmt.Errorf("unable to tokenize the code: %s", err)
+	}
+
+	commands, err := calculator.translator.Translate(tokens, functionsNames)
+	if err != nil {
+		return fmt.Errorf("unable to translate the tokens: %s", err)
+	}
+
+	err = calculator.evaluator.Evaluate(commands, variables, functions)
+	if err != nil {
+		return fmt.Errorf("unable to evaluate the commands: %s", err)
+	}
+
+	return nil
 }
