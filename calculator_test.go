57a60c530d52462bb6dd32c8f0fcfa92903b23b9
irenicaa <root@irenica.pro>
2020-08-23 17:11:05 +0300

    ...

--- calculator_test.go
+++ calculator_test.go
@@ -18,8 +18,8 @@ func TestCalculator(test *testing.T) {
 
 	testsCases := []struct {
 		name       string
-		args       args
 		fields     fields
+		args       args
 		wantNumber float64
 		wantErr    string
 	}{}
@@ -42,6 +42,45 @@ func TestCalculator(test *testing.T) {
 			} else {
 				assert.EqualError(test, gotErr, testCase.wantErr)
 			}
+		})
+	}
+}
+
+func TestCalculator_withSequentialCalls(test *testing.T) {
+	type fields struct {
+		variables map[string]float64
+		functions map[string]models.Function
+	}
+	type args struct {
+		codeParts []string
+	}
+
+	testsCases := []struct {
+		name       string
+		fields     fields
+		args       args
+		wantNumber float64
+	}{}
+	for _, testCase := range testsCases {
+		test.Run(testCase.name, func(test *testing.T) {
+			gotNumber, gotErr := 0.0, error(nil)
+
+			calculator := NewCalculator(
+				testCase.fields.variables,
+				testCase.fields.functions,
+			)
+			for _, codePart := range testCase.args.codeParts {
+				gotErr = calculator.Calculate(codePart)
+				if gotErr != nil {
+					break
+				}
+			}
+			if gotErr == nil {
+				gotNumber, gotErr = calculator.Finalize()
+			}
+
+			assert.Equal(test, testCase.wantNumber, gotNumber)
+			assert.NoError(test, gotErr)
 		})
 	}
 }
